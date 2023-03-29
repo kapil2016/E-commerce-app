1d0fab762f19d051cbb684fd34271a42aa420f78
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-03-29 20:12:01 +0530

    error handling

--- src/Entertainment/MoviesList.js
+++ src/Entertainment/MoviesList.js
@@ -1,40 +1,69 @@
-import React from 'react';
-import MovieCard from './MovieCard';
-import { Container, Row , Col, Button } from 'react-bootstrap';
-import { useState } from 'react';
+import React from "react";
+import MovieCard from "./MovieCard";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useState } from "react";
 
-async function fetchMovie(setMovies , setIsLoading){
+async function fetchMovie(setMovies, setIsLoading, setError) {
+  try {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films')
-    const data = await response.json()
-    const movies = data.results.map((item)=>{
-        return {id:item.release_date , title: item.title , description : item.opening_crawl, releaseDate:item.release_date , imageUrl:'https://xyz'}
-    })
+    setError(null);
+    const response = await fetch("https://swapi.dev/api/films");
+    if (!response.ok) {
+      throw new Error("Somthing Went Wrong");
+    }
+    const data = await response.json();
+    const movies = data.results.map((item) => {
+      return {
+        id: item.release_date,
+        title: item.title,
+        description: item.opening_crawl,
+        releaseDate: item.release_date,
+        imageUrl: "https://xyz",
+      };
+    });
     setMovies(movies);
     setIsLoading(false);
+  } catch (error) {
+    setIsLoading(false);
+    setError(error);
+  }
 }
 
 const MovieList = (props) => {
-  const [movies , setMovies] = useState([])
-  const[isLoading , setIsLoading] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  let loadingState = '';
-   if(isLoading){
-    loadingState = <p> Loading Movies  </p>
-   }
- 
+  let content = <p> No Movie Found </p>;
+  if (movies.length > 0) {
+    content = (
+      <Row className="row">
+        {movies.map((movie) => (
+          <Col className="col-md-6" key={movie.id}>
+            <MovieCard movie={movie} />
+          </Col>
+        ))}
+      </Row>
+    );
+  }
+
+  if (error) {
+    content = <p> {`${error}`} </p>;
+  }
+  if (isLoading) {
+    content = <p> Loading Movies </p>;
+  }
 
   return (
     <Container>
-    <Button onClick={()=>{fetchMovie(setMovies, setIsLoading)}}>Fetch Movie </Button>
-    {loadingState}
-    <Row className="row">
-      {movies.map((movie) => (
-        <Col className="col-md-6" key={movie.id}>
-          <MovieCard movie = {movie} />
-        </Col>
-      ))}
-    </Row >
+      <Button
+        onClick={() => {
+          fetchMovie(setMovies, setIsLoading, setError);
+        }}
+      >
+        Fetch Movie
+      </Button>
+      {content}
     </Container>
   );
 };
